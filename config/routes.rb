27b9fd5cc02264ced092ed9ddf9aa00def61b2f7
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-06-06 21:53:07 -0700

    sessions view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions
   get '/login', to: 'sessions#new'
 end
