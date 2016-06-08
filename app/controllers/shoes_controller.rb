ff1a0e310e1747e10df2830ef0074111bb2d5fd2
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-06-08 01:10:39 -0700

    update bootstrap gem and fix login

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,17 +1,20 @@
 class ShoesController < ApplicationController
   before_action :set_shoe, only: [:show, :edit, :update, :destroy]
+  before_action :owned_shoe, only: [:edit, :update, :destroy]
   def index
     @shoes = Shoe.all
     @shoe = Shoe.new
   end
+  def show
 
+  end
   def new
     @shoe = Shoe.new
   end
   def create
     if @shoe = Shoe.create(shoe_params)
       flash[:success] = "Your kicks has been created brah!"
-      redirect_to shoes_path
+      redirect_to root_path
     else
       flash.now[:alert] = "Your new post couldn't be created!"
       render :new
@@ -22,7 +25,7 @@ class ShoesController < ApplicationController
   def update
     if @shoe.update(shoe_params)
       flash[:success] = "Updated"
-      redirect_to shoes_path
+      redirect_to root_path
     else
       flash.now[:alert] = "Update failed!"
       render :edit
